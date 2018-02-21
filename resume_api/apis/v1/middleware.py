7952c7a897f4c978064615c290bb57675e855a63
Joe <joe.ekiert@gmail.com>
2018-02-20 18:07:19 -0800

    adding new middleware method to grab api key on request

--- resume_api/apis/v1/middleware.py
+++ resume_api/apis/v1/middleware.py
@@ -1,9 +1,10 @@
 import json
 
-from flask import current_app, request
+from flask import current_app, request, g
 from werkzeug.wrappers import Response
 from werkzeug.exceptions import HTTPException
 
+from resume_api.models import Client
 
 def check_auth(auth):
     un = current_app.config['BASIC_AUTH_USERNAME']
@@ -24,3 +25,21 @@ def basic_auth(endpoint):
             status=401
         )
         raise HTTPException('Api Error', response=resp)
+
+
+def get_api_client_from_request(endpoint):
+    """Ensure a valid api_key query param was provided with the request and
+    store the client on `g.api_client`
+    """
+
+    api_key = request.args.get('api_key', None)
+    if not api_key:
+        payload = { 'message': 'Missing required ?api_key param' }
+        endpoint.return_error(422, payload=payload)
+
+    client = Client.query.filter_by(client_id=api_key).one_or_none()
+    if client is None:
+        payload = { 'message': 'API Client not found' }
+        endpoint.return_error(404, payload=payload)
+
+    g.api_client = client
