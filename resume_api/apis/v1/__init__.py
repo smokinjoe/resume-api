8be5cbbf0e8237dd0b79cbfd38d07050108616f8
Joe <joe.ekiert@gmail.com>
2018-02-20 18:09:01 -0800

    adding the get_api_from_client method to before_all_hooks

--- resume_api/apis/v1/__init__.py
+++ resume_api/apis/v1/__init__.py
@@ -1,10 +1,15 @@
 from arrested import ArrestedAPI
 from .users import users_resource
 from .projects import projects_resource
-from .middleware import basic_auth
+from .middleware import basic_auth, get_api_client_from_request
 
-from .middleware import basic_auth
+api_v1 = ArrestedAPI(
+    url_prefix='/v1',
+    before_all_hooks=[
+    basic_auth,
+    get_api_client_from_request
+    ]
+)
 
-api_v1 = ArrestedAPI(url_prefix='/v1', before_all_hooks=[basic_auth])
 api_v1.register_resource(users_resource, defer=True)
 api_v1.register_resource(projects_resource, defer=True)
