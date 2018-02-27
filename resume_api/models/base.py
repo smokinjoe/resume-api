f7efc6cf29dd8e4dc46429c088249c621c96a394
Joe <joe.ekiert@gmail.com>
2018-02-26 19:29:42 -0800

    WIP: adding a handful of new methods for models to use

--- resume_api/models/base.py
+++ resume_api/models/base.py
@@ -1,9 +1,32 @@
 import datetime
-
+from collections import OrderedDict
 from sqlalchemy.ext.declarative import declared_attr
 
 from . import db
 
+def dump_datetime(value):
+    """Deserialize datetime object into string form for JSON processing."""
+    if value is None:
+        return None
+    return [value.strftime("%Y-%m-%d"), value.strftime("%H:%M:%S")]
+
+
+# Source: http://piotr.banaszkiewicz.org/blog/2012/06/30/serialize-sqlalchemy-results-into-json/
+class DictSerializable(object):
+    def _asDict(self):
+        result = OrderedDict()
+        for key in self.__mapper__.c.keys():
+            result[key] = getattr(self, key)
+        return result
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'id'         : self.id,
+           'created_at': dump_datetime(self.created_at),
+           'updated_at': dump_datetime(self.updated_at)
+       }
 
 class BaseMixin(object):
 
