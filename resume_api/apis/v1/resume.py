a877ec99f4420676465da771633c41871fd7c988
Joe <joe.ekiert@gmail.com>
2018-02-27 15:37:51 -0800

    able to dump the entire resume!

--- resume_api/apis/v1/resume.py
+++ resume_api/apis/v1/resume.py
@@ -1,42 +1,21 @@
 from arrested import Resource, Endpoint, GetObjectMixin, GetListMixin
 from flask import jsonify
-# from arrested.contrib.kim_arrested import KimEndpoint
-# from arrested.contrib.sql_alchemy import DBObjectMixin
 
 from resume_api.models import db, Project, EmploymentExperience, School, TechnicalExperience, User, WeaponOfChoice
-# from .mappers import ProjectMapper, EmploymentExperienceMapper, SchoolMapper, TechnicalExperienceMapper, UserMapper, WeaponOfChoiceMapper
 
 resume_resource = Resource('resume', __name__, url_prefix='/resume')
 
 class ResumeIndexEndpoint(Endpoint, GetObjectMixin):
     name = 'object'
-    # name = 'list'
-    # many = True
 
     def get_object(self):
-        # stmt = {
-        #     "projects": jsonify(json_list=[i.serialize for i in Project.query.all()]),
-        #     "employmentExperiences": jsonify(json_list=[i.serialize for i in EmploymentExperience.query.all()]),
-        #     "schools": jsonify(json_list=[i.serialize for i in School.query.all()]),
-        #     "technicalExperiences": jsonify(json_list=[i.serialize for i in TechnicalExperience.query.all()]),
-        #     "user": jsonify(json_list=[i.serialize for i in User.query.all()]),
-        #     "weaponsOfChoice": jsonify(json_list=[i.serialize for i in WeaponOfChoice.query.all()])
-        # }
-        # return jsonify(result=stmt)
-        # stmt = {
-        #     "projects": Project.query.all(),
-        #     "employmentExperiences": EmploymentExperience.query.all(),
-        #     "schools": School.query.all(),
-        #     "technicalExperiences": TechnicalExperience.query.all(),
-        #     "user": User.query.all(),
-        #     "weaponsOfChoice": WeaponOfChoice.query.all()
-        # }
-        # return stmt
         stmt = {
-            "projects": {
-                "id": 1,
-                "title": "Lorem ipsum"
-            }
+            "projects": [i.serialize for i in Project.query.all()],
+            "employmentExperiences": [i.serialize for i in EmploymentExperience.query.all()],
+            "schools": [i.serialize for i in School.query.all()],
+            "technicalExperiences": [i.serialize for i in TechnicalExperience.query.all()],
+            "user": [i.serialize for i in User.query.all()],
+            "weaponsOfChoice": [i.serialize for i in WeaponOfChoice.query.all()]
         }
         return stmt
 
