6af799f30eb8fca789b1f2b3f480369e3356bc7d
Joe <joe.ekiert@gmail.com>
2018-02-27 15:51:57 -0800

    implementing auth layer again!

--- resume_api/app.py
+++ resume_api/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_alembic import Alembic
 from flask_cors import CORS
 from resume_api.models import db
-from resume_api.apis import api_v1
+from resume_api.apis import api_v1, api_v1_w_auth
 
 def create_app():
 
@@ -10,7 +10,10 @@ def create_app():
     app.config.from_object('resume_api.config.settings')
 
     db.init_app(app)
+
     api_v1.init_app(app)
+    api_v1_w_auth.init_app(app)
+
     Alembic(app)
     CORS(app)
 
