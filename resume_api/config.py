2528bf0721c65b5c7f0dbbdcc984cc0660f288b0
Joe <joe.ekiert@gmail.com>
2018-03-16 18:36:16 -0700

    removing unused utils/client model and old config consts

--- resume_api/config.py
+++ resume_api/config.py
@@ -16,8 +16,6 @@ class Dev(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:////opt/code/resume_api.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    BASIC_AUTH_USERNAME = 'admin'
-    BASIC_AUTH_PASSWORD = 'secret'
     SECRET_KEY = THE_SECRET_KEY
 
 settings = globals()[os.environ.get('FLASK_CONFIG', 'Dev')]
