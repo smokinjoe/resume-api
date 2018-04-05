82f51c298fc17385935f656bf3691ee4a82f114d
Joe <joe.ekiert@gmail.com>
2018-04-04 19:01:20 -0700

    what is going on

--- resume_api/app.py
+++ resume_api/app.py
@@ -18,4 +18,8 @@ def create_app():
     Alembic(app)
     CORS(app)
 
+    # if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
     return app
--- resume_api/wsgi.py
+++ resume_api/wsgi.py
@@ -1,7 +1,3 @@
 from resume_api.app import create_app  # pragma: no cover
 
 app = create_app()  # pragma: no cover
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
