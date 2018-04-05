6ff7c83adc23ec69cded744be4b0e7d8001c9caa
Joe <joe.ekiert@gmail.com>
2018-04-04 18:18:26 -0700

    confoused

--- resume_api/app.py
+++ resume_api/app.py
@@ -18,11 +18,7 @@ def create_app():
     Alembic(app)
     CORS(app)
 
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
     return app
-
-
-
-
